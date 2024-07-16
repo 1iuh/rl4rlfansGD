f0708ca022536bce1feaa395ab1e52b4ad10c0fd
Halcyon <53889644+Golden-kitty@users.noreply.github.com>
2024-07-16 22:35:22 +0800

    Newmap_demosewermap
    add a demo map

---
+++ Src/Map/WFC/map_demo_sewer_nearsurface.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal next_step
+var eraseID:Vector2i = Vector2i(0,1)
+@onready var input_handler = $MainGameInputHandler
+@export var target:TileMap
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$generator/Sample.hide()
+	$generator/Sample_negative.hide()
+	$generator/Target.show()
+	$generator.start()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func _physics_process(delta:float)->void:
+	if Input.is_action_just_pressed("wait"):
+		next_step.emit()
+	var entity: Entity = null
+	var action: Action = input_handler.get_action(entity)
+	if action:
+		action.perform()
